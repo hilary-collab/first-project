4d6d6b6bec3d04f4fa0767305efbbb70a0c7a239
petiteUnicone <prajnanupam@gmail.com>
2026-08-29 12:07:34 +0100

    update

--- trial.py
+++ trial.py
@@ -1,13 +1,21 @@
+import pygame 
 
 print('7:55 Monday morning ')
 print('Your backpack feels heavy. Your stomache is aching. It is your first day to a brand new school !')
 print('The bell is ringing in five minutes. ')
 print('You were told to reach to room 242 to meet Ms Smith before the first bell')
 print('But are you ?')
+
 name=input('What is your name?')
+
 print( name," welcome to the 'Try to be perfect school'")
 print('Just like the name of our school, parents send their children here to become perfect')
 print('You are standing in the front entrance of the school')
-print('1) Going forward you can easily find room 242, but we have no clue if Ms Smith going to gentle to you.')
+
+print('1) Going forward you can easily find room 242, but we have no clue if Ms Smith is going to be nice.')
 print('2) Going to the left, you can simply escape school')
- choice1 =int(input("What's your choice?\n"))
+choice1 =int(input("What's your choice?\n"))
+if choice1== 1:
+ print('Choice 1')
+else:
+ print('Choice2')
