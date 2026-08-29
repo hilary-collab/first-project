f628adea4e0277e3f71d24d8d3bf1ce9789017d5
petiteUnicone <prajnanupam@gmail.com>
2026-08-29 11:03:11 +0100

    background

--- trial.py
+++ trial.py
@@ -1,4 +1,10 @@
 
 print('7:55 Monday morning ')
+print('Your backpack feels heavy. Your stomache is aching. It is your first day to a brand new school !')
+print('The bell is ringing in five minutes. ')
+print('You were told to reach to room 242 to meet Ms Smith before the first bell')
+print('But are you ?')
 name=input('What is your name?')
-print(name)
+print(name,"welcome to the 'Try to be perfect school'")
+print('Just like the name of our school, parents send their children here to become perfect')
+print()
